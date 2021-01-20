79228ff2a6c7e675864d7bd2fe87cc885030e8cc
Youngjoon Lee <taxihighway@gmail.com>
2021-01-21 00:16:41 +0900

    font and yay packages

--- archlinux/3_post.sh
+++ archlinux/3_post.sh
@@ -4,4 +4,17 @@ set -euxo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-${DIR}/../dotfiles/install.sh
+#${DIR}/../dotfiles/install.sh
+
+sudo pacman -S go nodejs yarn jdk-openjdk
+sudo pacman -S code alacritty
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+mkdir -p ~/repos
+git clone https://aur.archlinux.org/yay.git ~/repos/yay
+cd ~/repos/yay
+makepkg -si
+cd -
+
+yay -S intellij-idea-ultimate-edition
