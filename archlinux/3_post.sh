a5e83b1b8870843d68b81a40b4f2c1d1f39985cc
Youngjoon Lee <taxihighway@gmail.com>
2021-01-23 23:35:13 +0900

    visual-studio-code-bin instead of code

--- archlinux/3_post.sh
+++ archlinux/3_post.sh
@@ -6,8 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #${DIR}/../dotfiles/install.sh
 
-sudo pacman -S go nodejs yarn jdk-openjdk
-sudo pacman -S code alacritty
+sudo pacman -S go nodejs yarn jdk-openjdk alacritty
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
@@ -17,5 +16,5 @@ cd ~/repos/yay
 makepkg -si
 cd -
 
-yay -S intellij-idea-ultimate-edition
+yay -S visual-studio-code-bin intellij-idea-ultimate-edition
 yay -S ttf-nanum
